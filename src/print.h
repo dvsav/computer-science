03c47a47d93fcc1aac3ec0c6e2398a0a0a8139ba
dvsav <dvsav.spels@gmail.com>
2023-10-22 22:07:37 +0400

    replace Tabs with spaces

--- src/print.h
+++ src/print.h
@@ -2,10 +2,10 @@
 
 namespace sc
 {
-	template <typename TIterator>
-	void print(TIterator begin, TIterator end, const std::string& delimeter)
-	{
-		for (auto i = begin; i != end; i++)
-			std::cout << *i << delimeter;
-	}
+    template <typename TIterator>
+    void print(TIterator begin, TIterator end, const std::string& delimeter)
+    {
+        for (auto i = begin; i != end; i++)
+            std::cout << *i << delimeter;
+    }
 }
