05e1d9697114f7d82c0f669a52484b843e8a46f4
dvsav <dvsav.spels@gmail.com>
2024-01-10 22:42:50 +0400

    huffman_encoding.h: add Doxygen comments

--- src/huffman_encoding.h
+++ src/huffman_encoding.h
@@ -5,11 +5,22 @@
 #include <map>     // for std::map
 #include <queue>   // for std::priority_queue
 #include <stack>   // for std::stack
-#include <utility> // for std::pair
+#include <utility> // for std::pair, std::move
 #include <vector>  // for std::vector
 
 namespace cs
 {
+    /**
+     * @class HuffmanEncoding
+     * @brief Class providing Huffman encoding algorithm (so called prefix-free encoding).
+     * Allows for encoding symbols of some alphabet. Each symbol has certain
+     * associated frequency (what the probability is of encountering that symbol in a text).
+     * A symbol is encoded to a set of bits. Length of this set of bits
+     * depends on the frequency of the symbol - the more frequent the symbol, the shorter
+     * the length of its encoding (set of bits).
+     * @tparam TSymbol symbol type.
+     * @tparam TFrequency symbol frequency (probability) type.
+     */
     template <typename TSymbol, typename TFrequency>
     class HuffmanEncoding final
     {
@@ -39,11 +50,26 @@ namespace cs
         std::map<TSymbol, HuffmanSymbol*> leaves;
 
     public:
+        /**
+         * @brief Creates a new Huffman encoding from a specified @p alphabet.
+         * @param alphabet
+         */
         HuffmanEncoding(
             std::vector<std::pair<TSymbol, TFrequency> >& alphabet) :
             root(nullptr),
             leaves()
         {
+            // Algorithm:
+            // Create a binary tree structure with the symbols of the alphabet
+            // stored in the leaves of the tree. Binary encoding of a particular
+            // symbol is obtained by traversing from the root of the tree to the
+            // leaf storing the symbol where each edge denoted either 0 (if it
+            // leads to a left child) or 1 (if it leads to a right child).
+            // The less frequent symbols are stored in deeper leaves (therefore
+            // they have longer binary encodings) while more frequent symbols are
+            // stored closer to the root of the tree (therefore they have shorter
+            // binary encodings).
+
             std::priority_queue<HuffmanSymbol*, std::vector<HuffmanSymbol*>, SymbolComparator> minHeap;
             for (const std::pair<TSymbol, TFrequency>& symbol_freq : alphabet)
             {
@@ -108,7 +134,24 @@ namespace cs
             }
         }
 
+        // Move constructor.
+        HuffmanEncoding(HuffmanEncoding&& rval) :
+            root(rval.root),
+            leaves(std::move(rval.leaves))
+        {
+            rval.root = nullptr;
+        }
+
+        HuffmanEncoding(const HuffmanEncoding&) = delete;
+        HuffmanEncoding& operator=(const HuffmanEncoding&) = delete;
+
     public:
+        /**
+         * @brief Returns @p symbol encoding as a vector of boolean values
+         * which play a role of bits in binary encoding.
+         * @param symbol - symbol to be encoded.
+         * @return symbol's binary encoding.
+         */
         std::vector<bool> Encode(TSymbol symbol) const
         {
             HuffmanSymbol* current_node = leaves.at(symbol);
@@ -124,6 +167,12 @@ namespace cs
             return encoded;
         }
 
+        /**
+         * @brief Decodes some binary encoding represented as a vector of boolean
+         * values to a symbol.
+         * @param code - symbol's binary encoding.
+         * @return decoded symbol.
+         */
         TSymbol Decode(const std::vector<bool>& code)
         {
             HuffmanSymbol* current_node = root;
