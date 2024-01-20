0379cef912bfce2c02d79e4dab8383a3e4f5b4be
dvsav <dvsav.spels@gmail.com>
2024-01-21 00:30:03 +0400

    knapsack.h: add Doxygen comments

--- src/knapsack.h
+++ src/knapsack.h
@@ -8,6 +8,13 @@
 
 namespace cs
 {
+    /**
+     * @class KnapsackItem
+     * @brief Represents an item that can be put into
+     * a knapsack (backpack). Characterized by weight
+     * (integer variable) and value.
+     * @tparam TValue value type.
+     */
     template <typename TValue>
     struct KnapsackItem
     {
@@ -22,6 +29,17 @@ namespace cs
         {}
     };
 
+    /**
+     * @brief Solves the so called 'knapsack problem'.
+     * The goal of this algorithm is to select items from a
+     * specified set @p items which together make maximal
+     * possible value and whose total weight doesn't exceed
+     * @p maxWeight. Complexity: O(items.size() * maxWeight)
+     * @param maxWeight - maximum weight that the knapsack can carry.
+     * @param items - set of items from which we need to select.
+     * @return selected items that together make maximal possible
+     * value while not exceeding the maximum weight of the knapsack.
+     */
     template <typename TValue>
     std::vector<KnapsackItem<TValue> > PackKnapsack(
         int maxWeight,
@@ -29,8 +47,20 @@ namespace cs
     {
         Requires::ArgumentPositive(maxWeight, NAMEOF(maxWeight), FUNCTION_INFO);
 
+        // Knapsack problem's solution is an example of dynamic programming concept.
+        // We have n items indexed from i=0 to i=n-1.
+        // The full problem is reduced to the following two subproblems:
+        // 1. If we take item i=n-1, solve the problem for n-1 items (i=0 to i=n-2) and max weight = maxWeight - (n-1)th item's weight.
+        // 2. If we don't take item i=n-1, solve the problem for n-1 items (i=0 to i=n-2) and max weight = maxWeight.
+        // From the two subproblems above pick the one giving the most total value of items.
+
+        // We allocate an 2D array A with n rows and maxWeight columns.
+        // A[i, x] where (i=0...n-1 and x=1...maxWeight) is the solution (total value)
+        // of a subproblem for items={item[0]...item[i]} and max weight=x.
         const int n = items.size();
         std::unique_ptr<TValue> A(new TValue[maxWeight * n]);
+
+        // Function that returns the A[i, x] element of array A (by value).
         auto a = [&A, maxWeight](int i, int x) -> TValue
         {
             if (i < 0 || x < 1)
@@ -41,6 +71,7 @@ namespace cs
             return A.get()[i * maxWeight + x - 1];
         };
 
+        // Function that returns the A[i, x] element of array A (by reference).
         auto a_ref = [&A, maxWeight](int i, int x) -> TValue&
         {
             // i = [0...n)
@@ -48,6 +79,7 @@ namespace cs
             return A.get()[i * maxWeight + x - 1];
         };
 
+        // Solve all the subproblens from smallest to the largest.
         for (int i = 0; i < n; ++i)
         {
             for (int x = 1; x <= maxWeight; ++x)
@@ -66,7 +98,11 @@ namespace cs
             }
         }
 
-        // Reconstruction
+        // Now we have the filled array A of subproblem solutions,
+        // so we know the maximal value, but we don't know the set of
+        // items that led to that value. So we need to do another pass
+        // (called "reconstruction") from the full problem value A[n-1, maxWeight]
+        // to the smallest subproblem A[0, ...].
         std::vector<KnapsackItem<TValue> > knapsack;
         int x = maxWeight;
         for (int i = n - 1; i >= 0; --i)
