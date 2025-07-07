32f45d51aa7d1c753a9f280291aaa8324618a11a
Dmitry Savchenkov <dvsav.spels@gmail.com>
2025-07-07 15:45:10 +0300

    Add comments to CMake stuff

--- make.sh
+++ make.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 SCRIPT_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-BLD_DIR="${SCRIPT_DIR}/build/GNUMake"
 
+# Build directory contains the Makefile script for GNU Make build utility
+BUILD_DIR="${SCRIPT_DIR}/build/GNUMake"
+
+# Build the targets defined in the Makefile
+# passing any command line arguments to GNU Make via $@
 echo "make $@"
-make -C "$BLD_DIR" $@
+make -C "$BUILD_DIR" $@
